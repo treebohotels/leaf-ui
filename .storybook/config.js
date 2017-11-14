5c52c607c833c6659c561a0c7078196e8058f4c3
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-11-14 13:17:10 +0530

    add @storybook/addon-options, @storybook/addon-actions, @storybook/addon-knobs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { configure, addDecorator } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+import { withKnobs } from '@storybook/addon-knobs';
 import theme from '../src/theme';
 
+setOptions({
+  name: 'Leaf-UI',
+  url: 'https://github.com/treebohotels/leaf-ui.git',
+  downPanelInRight: true,
+});
+addDecorator(withKnobs);
 addDecorator(story => (
   <ThemeProvider theme={theme}>
     {story()}
