f355be35985cfb071ae75dfdf00debb30e03356c
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-11-15 17:34:32 +0530

    update Card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Card = styled.section`
-  background: ${(props) => props.theme.color[props.color]};
+const Card = styled.div`
+  background: ${(props) => props.theme.color[props.backgroundColor]};
   border-radius: ${(props) => props.theme.border.radius};
   box-shadow: ${(props) => props.theme.shadow[props.elevation]};
 `;
 
 Card.propTypes = {
-  color: PropTypes.string,
+  backgroundColor: PropTypes.string,
   elevation: PropTypes.number,
 };
 
 Card.defaultProps = {
-  color: 'white',
+  backgroundColor: 'white',
   elevation: 0,
 };
 
---
+++ src/Card/Card.story.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { select, number } from '@storybook/addon-knobs';
+import theme from '../theme';
+import Card from './Card';
+
+storiesOf('Card', module)
+  .add('playground', () => (
+    <Card
+      style={{ width: '120px', height: '120px' }}
+      backgroundColor={select('backgroundColor', Object.keys(theme.color), 'white')}
+      elevation={number('elevation', 0)}
+    >
+      Card content
+    </Card>
+  ));
