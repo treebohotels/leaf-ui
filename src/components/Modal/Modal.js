9141233c3b7914ea64d33340dc39f2aa83f5a52e
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2017-11-23 14:02:11 +0530

    accept container for Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -7,11 +7,11 @@ import ModalContent from './ModalContent';
 import ModalFooter from './ModalFooter';
 
 const backdropStyle = {
-  position: 'fixed',
+  position: 'absolute',
   top: 0,
-  bottom: 0,
-  left: 0,
   right: 0,
+  left: 0,
+  bottom: 0,
   zIndex: 'auto',
   background: 'rgba(0, 0, 0, 0.25)',
 };
@@ -22,15 +22,20 @@ const Dialog = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   box-shadow: ${(p) => p.theme.boxShadow[3]};
-  min-width: ${(p) => p.theme.px(70)};
+  min-width: ${(p) => p.theme.px(75)};
 `;
 
-const Modal = ({ isOpen, onClose, children }) => (
+const Modal = ({
+  isOpen,
+  onClose,
+  container,
+  children,
+}) => (
   <ReactOverlaysModal
     backdropStyle={backdropStyle}
     show={isOpen}
     onHide={onClose}
-    autoFocus={false}
+    container={container}
   >
     <Dialog>
       {
@@ -47,6 +52,7 @@ const Modal = ({ isOpen, onClose, children }) => (
 Modal.propTypes = {
   isOpen: PropTypes.bool,
   onClose: PropTypes.func,
+  container: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
   children: PropTypes.node.isRequired,
 };
 
