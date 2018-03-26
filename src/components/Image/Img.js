09071e2918a6d1244246c7bcf5e5f4bd0f551955
Himanshu Gupta <himanshu.gupta@treebohotels.com>
2018-03-26 19:58:05 +0530

    style(image): removed shape props from img.js

--- src/components/Image/Img.js
+++ src/components/Image/Img.js
@@ -1,26 +1,12 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const styles = {
-  borderRadius(props) {
-    if (props.shape === 'bluntEdged') {
-      return props.theme.borderRadius;
-    } else if (props.shape === 'sharpEdged') {
-      return '0';
-    } else if (props.shape === 'circular') {
-      return '100%';
-    }
-    return '';
-  },
-};
-
 const Img = styled.img`
   width: ${(props) => props.width || ''};
   height: ${(props) => props.height || ''};
   position: ${(props) => props.isLoaded ? 'static' : 'initial'};
   visibility: ${(props) => props.src ? 'initial' : 'hidden'};
   filter: ${(props) => props.grayscale ? 'grayscale(1)' : 'grayscale(0)'};
-  border-radius: ${styles.borderRadius};
 `;
 
 Img.propTypes = {
@@ -29,16 +15,13 @@ Img.propTypes = {
   width: PropTypes.string,
   height: PropTypes.string,
   grayscale: PropTypes.bool,
-  shape: PropTypes.oneOf(['bluntEdged', 'sharpEdged', 'circular']),
   isLoaded: PropTypes.bool,
-  borderRadius: PropTypes.string,
 };
 
 Img.defaultProps = {
   width: '',
   height: '',
   grayscale: false,
-  shape: 'sharpEdged',
   isLoaded: false,
 };
 
