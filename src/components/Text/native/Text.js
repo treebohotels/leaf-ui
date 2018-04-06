fcd78e27ea664b645279be1e3e74dd65e92f8168
Kashish Grover <kashish.grover@treebohotels.com>
2018-04-06 11:40:05 +0530

    fix(native/text): fixed properties for native text component

--- src/components/Text/native/Text.js
+++ src/components/Text/native/Text.js
@@ -1,29 +1,38 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Platform } from 'react-native';
+
+const isPlatformAndroid = !(Platform.OS === 'ios');
+
+export const fontFamilies = {
+  normal: isPlatformAndroid ? 'Roboto' : 'SFProDisplay-Regular',
+  medium: isPlatformAndroid ? 'Roboto-Medium' : 'SFProDisplay-Medium',
+  semibold: isPlatformAndroid ? 'Roboto-Medium' : 'SFProDisplay-Semibold',
+  bold: 'Averta-Bold',
+};
 
 const Text = styled.Text`
-  ${(p) => p.color ? `color: ${p.theme.color[p.color]};` : ''}
-  ${(p) => p.size ? `font-size: ${p.theme.fontSize[p.size]};` : ''}
-  ${(p) => p.weight ? `font-weight: ${p.theme.fontWeight[p.weight]};` : ''}
-  ${(p) => {
-    if (p.family) {
-      return `font-family: ${p.family}`;
-    }
-    return `font-family: ${p.theme.fontFamily[p.weight]}`;
-  }}
-  `;
+  color: ${(props) => props.theme.color[props.color]};
+  font-size: ${(props) => props.theme.fontSize[props.size]};
+  font-weight: ${(props) => props.theme.fontWeight[props.weight]};
+  font-family: ${(props) => fontFamilies[props.family]};
+  text-align: ${(props) => props.align};
+`;
 
 Text.propTypes = {
   color: PropTypes.string,
-  size: PropTypes.string,
-  weight: PropTypes.string,
-  family: PropTypes.string,
-  truncate: PropTypes.bool,
+  size: PropTypes.oneOf(['xxxxl', 'xxxl', 'xxl', 'xl', 'l', 'm', 's', 'xs', 'xxs']),
+  weight: PropTypes.oneOf(['normal', 'medium', 'semibold', 'bold']),
+  family: PropTypes.oneOf(['normal', 'medium', 'semibold', 'bold']),
+  align: PropTypes.oneOf(['left', 'center', 'right']),
 };
 
 Text.defaultProps = {
-  size: 's',
+  color: 'greyDarker',
+  size: 'm',
   weight: 'normal',
+  family: 'normal',
+  align: 'left',
 };
 
 export default Text;
