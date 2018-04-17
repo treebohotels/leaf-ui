1d238a010208e91cd3625d2c12b09f49f44ae416
Kashish Grover <kashish.grover@treebohotels.com>
2018-04-17 16:54:03 +0530

    fix(Tag/native): update native tag wrt new guidelines

--- src/components/Tag/native/Tag.js
+++ src/components/Tag/native/Tag.js
@@ -3,37 +3,41 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Text from '../../Text/native';
 
+const styles = {
+  background(props) {
+    if (props.kind === 'filled') return props.theme.color[props.color];
+    return props.theme.color[`${props.color}Lighter`];
+  },
+  padding(props) {
+    if (props.size === 'small') return props.theme.px([0.15, 0.5, 0.25, 0.5]);
+    return props.theme.px([1, 1.5]);
+  },
+  borderColor(props) {
+    return props.theme.color[props.color];
+  },
+  borderRadius(props) {
+    if (props.shape === 'capsular') return props.theme.px(10);
+    return props.theme.borderRadius;
+  },
+};
+
 const TagContainer = styled.View`
   align-self: flex-start;
   align-items: center;
   justify-content: center;
-  ${(p) => ({
-    small: `
-      padding: ${p.theme.px(0.5)};
-    `,
-    medium: `
-      padding: ${p.theme.px([1, 1.5])};
-    `,
-  }[p.size])}
-  ${(p) => ({
-    filled: `
-      background: ${p.theme.color[p.color]};
-    `,
-    outlined: `
-      background: ${p.theme.color[`${p.color}Lighter`]};
-    `,
-  }[p.kind])}
-  border-radius: 100px;
-  border-width: 1px;
-  border-color: ${(p) => p.theme.color[p.color]};
-  `;
+  border-radius: ${styles.borderRadius};
+  padding: ${styles.padding};
+  background: ${styles.background};
+  border-width: 0.8;
+  border-color: ${styles.borderColor};
+`;
 
-const Tag = ({ style, color, kind, size, children }) => (
+const Tag = ({ color, kind, size, children, shape }) => (
   <TagContainer
-    style={style}
     color={color}
     kind={kind}
     size={size}
+    shape={shape}
   >
     <Text
       color={{
@@ -51,13 +55,10 @@ const Tag = ({ style, color, kind, size, children }) => (
 );
 
 Tag.propTypes = {
-  style: PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.object,
-  ]),
   color: PropTypes.string,
   kind: PropTypes.oneOf(['filled', 'outlined']),
   size: PropTypes.oneOf(['small', 'medium']),
+  shape: PropTypes.oneOf(['bluntEdged', 'capsular']),
   children: PropTypes.string,
 };
 
@@ -65,6 +66,7 @@ Tag.defaultProps = {
   color: 'primary',
   kind: 'outlined',
   size: 'medium',
+  shape: 'capsular',
 };
 
 export default Tag;
