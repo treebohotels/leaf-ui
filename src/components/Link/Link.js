b867172a20461b2f6a0b423b29ff5dbdbaa4e9de
Kamlesh Chandnani <kamlesh.chandnani@gmail.com>
2018-04-20 09:54:05 +0530

    fix(Link): set text-decoration to none (#38)

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Link = styled.a`
   color: ${(props) => props.theme.color.blue};
+  text-decoration: none;
 
   &:hover {
     color: ${(props) => props.theme.color.blueDark};
