16af015cfe599dc7830d4d2d2107034f2e11aca9
Kashish Grover <kashish.grover@treebohotels.com>
2018-04-24 00:28:08 +0530

    feat(Card/native): add native Card component (#42)

--- storybook/native/stories.js
+++ storybook/native/stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react-native';
 import styled from 'styled-components';
 import theme from '../../src/theme';
 import Button from '../../src/components/Button/native';
+import Card from '../../src/components/Card/native';
 import Text from '../../src/components/Text/native';
 import Form from '../../src/components/Form';
 import TextInput from '../../src/components/TextInput/native';
@@ -90,6 +91,54 @@ storiesOf('Button', module)
       <Button disabled>
         <Text>Disabled Button</Text>
       </Button>
+    </Spacer>
+  ));
+
+storiesOf('Card', module)
+  .add('Lagoon', () => (
+    <Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={0}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 0</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={1}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 1</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={2}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 2</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={3}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 3</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={4}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 4</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
+      <Spacer margin={[0, 0, 5, 0]}>
+        <Card color="lagoon" elevation={5}>
+          <Spacer padding={2}>
+            <Text color="white">Elevation: 5</Text>
+          </Spacer>
+        </Card>
+      </Spacer>
     </Spacer>
   ));
 
