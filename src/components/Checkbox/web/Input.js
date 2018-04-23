8d5411a297fc1c0fbe4dae7a28e63da737312776
Srinivas Damam <srinivasdamam@gmail.com>
2018-04-24 00:34:36 +0530

    fix(Checkbox, Radio): use clip-path in addition to clip (#27)
    closes #25

--- src/components/Checkbox/web/Input.js
+++ src/components/Checkbox/web/Input.js
@@ -48,7 +48,9 @@ const Input = styled.input.attrs({
   padding: 0;
   margin: 0;
   overflow: hidden;
+  /* For browsers that yet don't support clip-path */
   clip: rect(0, 0, 0, 0);
+  clip-path: inset(1px 0 0 0);
   border: 0;
   visibility: visible;
   white-space: nowrap;
