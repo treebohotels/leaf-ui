7491196db9b4a0bdbc72a6caf33057e49939be60
kamleshchandnani <kamlesh.chandnani@gmail.com>
2018-04-27 15:22:36 +0530

    fix(MoreOrLess): change dummy text to string
    I was dumb enough to not think about react-native :D

--- src/MoreOrLess/makeStories.js
+++ src/MoreOrLess/makeStories.js
@@ -1,47 +1,46 @@
 import React, { Fragment } from 'react';
 
-const LoremIpsum = () => (
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-    Maecenas at elit eget diam convallis ultricies in a ligula.<br />
-    Nunc rutrum est ut risus laoreet laoreet.<br />
-    Donec convallis massa at pretium tincidunt.<br />
-    Quisque quis tellus quis odio venenatis bibendum.<br /> <br />
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-    Maecenas at elit eget diam convallis ultricies in a ligula.<br />
-    Nunc rutrum est ut risus laoreet laoreet.<br />
-    Donec convallis massa at pretium tincidunt.<br />
+const sampleText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas at elit eget diam convallis ultricies in a ligula.
+    Nunc rutrum est ut risus laoreet laoreet.
+    Donec convallis massa at pretium tincidunt.
+    Quisque quis tellus quis odio venenatis bibendum. 
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas at elit eget diam convallis ultricies in a ligula.
+    Nunc rutrum est ut risus laoreet laoreet.
+    Donec convallis massa at pretium tincidunt.
     Quisque quis tellus quis odio venenatis bibendum.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-    Maecenas at elit eget diam convallis ultricies in a ligula.<br />
-    Nunc rutrum est ut risus laoreet laoreet.<br />
-    Donec convallis massa at pretium tincidunt.<br />
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas at elit eget diam convallis ultricies in a ligula.
+    Nunc rutrum est ut risus laoreet laoreet.
+    Donec convallis massa at pretium tincidunt.
     Quisque quis tellus quis odio venenatis bibendum.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-    Maecenas at elit eget diam convallis ultricies in a ligula.<br />
-    Nunc rutrum est ut risus laoreet laoreet.<br />
-    Donec convallis massa at pretium tincidunt.<br />
-    Quisque quis tellus quis odio venenatis bibendum.
-  </div>
-);
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Maecenas at elit eget diam convallis ultricies in a ligula.
+    Nunc rutrum est ut risus laoreet laoreet.
+    Donec convallis massa at pretium tincidunt.
+    Quisque quis tellus quis odio venenatis bibendum`;
 
 export default (storiesOf, { MoreOrLess, Card, Spacer }) =>
-  storiesOf('MoreOrLess', module)
-    .add('simple', () => (
-      <Fragment>
-        <Spacer padding={[2]}>
-          <MoreOrLess initialHeight="100px" labelForMore="Show More" labelForLess="Show Less">
-            <Card>
-              <LoremIpsum />
-            </Card>
-          </MoreOrLess>
-        </Spacer>
-        <Spacer padding={[2]}>
-          <MoreOrLess initialHeight="100px" labelForMore="Show More" labelForLess="Show Less">
-            <Card>
-              <LoremIpsum />
-            </Card>
-          </MoreOrLess>
-        </Spacer>
-      </Fragment>
-    ));
+  storiesOf('MoreOrLess', module).add('simple', () => (
+    <Fragment>
+      <Spacer padding={[2]}>
+        <MoreOrLess
+          initialHeight="100px"
+          labelForMore="Show More"
+          labelForLess="Show Less"
+        >
+          <Card>{sampleText}</Card>
+        </MoreOrLess>
+      </Spacer>
+      <Spacer padding={[2]}>
+        <MoreOrLess
+          initialHeight="100px"
+          labelForMore="Read More"
+          labelForLess="Read Less"
+        >
+          <Card>{sampleText}</Card>
+        </MoreOrLess>
+      </Spacer>
+    </Fragment>
+  ));
