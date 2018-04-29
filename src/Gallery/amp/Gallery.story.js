fbeff60f8d342379f907a08e3eac2bb4545f074b
kamleshchandnani <kamlesh.chandnani@gmail.com>
2018-04-29 18:01:10 +0530

    fix(Gallery): remove unused imports

--- src/Gallery/amp/Gallery.story.js
+++ src/Gallery/amp/Gallery.story.js
@@ -1,11 +1,9 @@
 import { storiesOf } from '@storybook/react';
 import Image from '../../Image/amp';
 import Gallery from '../amp';
-import Spacer from '../../Spacer/amp';
 import makeStories from '../makeStories';
 
 makeStories(storiesOf, {
   Gallery,
   Image,
-  Spacer,
 });
--- src/Gallery/web/Gallery.story.js
+++ src/Gallery/web/Gallery.story.js
@@ -1,11 +1,9 @@
 import { storiesOf } from '@storybook/react';
 import Image from '../../Image/web';
 import Gallery from '../web';
-import Spacer from '../../Spacer/web';
 import makeStories from '../makeStories';
 
 makeStories(storiesOf, {
   Gallery,
   Image,
-  Spacer,
 });
