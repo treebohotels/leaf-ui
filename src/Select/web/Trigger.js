9d878478a21242875221f8216d6a2da4b32774ea
Srinivas Damam <srinivasdamam@gmail.com>
2018-04-29 10:24:55 +0530

    fix(Select): Fixes Select not receiving keyboard events
    #50

--- src/Select/web/Trigger.js
+++ src/Select/web/Trigger.js
@@ -1,6 +1,18 @@
-import Flex from '../../Flex/web';
+import styled from 'styled-components';
+import Label from './Label';
 
 const styles = {
+  focus: {
+    borderColor(props) {
+      return props.theme.color.primary;
+    },
+    backgroundColor(props) {
+      return props.theme.color.white;
+    },
+    color(props) {
+      return props.theme.color.primary;
+    },
+  },
   borderColor(props) {
     if (props.isOpen) {
       return props.theme.color.primary;
@@ -23,7 +35,7 @@ const styles = {
   },
 };
 
-const Trigger = Flex.extend`
+const Trigger = styled.button`
   position: relative;
   display: inline-flex;
   justify-content: space-between;
@@ -35,6 +47,16 @@ const Trigger = Flex.extend`
   padding: ${(props) => props.theme.px([3, 1.5, 1.5, 1.5])};
   border: 1px solid ${styles.borderColor};
   border-radius: ${(props) => props.theme.borderRadius};
+
+  &:focus {
+    background-color: ${styles.focus.backgroundColor};
+    border-color: ${styles.focus.borderColor};
+    outline: none;
+  }
+
+  &:focus > ${/* sc-selector */Label} {
+    color: ${styles.focus.color};
+  }
 `;
 
 export default Trigger;
