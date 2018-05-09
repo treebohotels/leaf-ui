b406bb0ad10383450dca2fa4cb771210415b03ba
kamlesh_chandnani <kamlesh.chandnani@gmail.com>
2018-05-09 16:49:44 +0530

    fix(utils): add check to avoid nulls while conditional rendering

--- src/utils/reactChildrenGroupByType.js
+++ src/utils/reactChildrenGroupByType.js
@@ -3,8 +3,10 @@ import React from 'react';
 export default (children) => {
   const childrenGroupByType = {};
   React.Children.forEach(children, (child) => {
-    childrenGroupByType[child.type] = childrenGroupByType[child.type] || [];
-    childrenGroupByType[child.type].push(child);
+    if (child) {
+      childrenGroupByType[child.type] = childrenGroupByType[child.type] || [];
+      childrenGroupByType[child.type].push(child);
+    }
   });
   return childrenGroupByType;
 };
