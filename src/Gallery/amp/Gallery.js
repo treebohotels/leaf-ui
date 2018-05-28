bc834e5908af7603a1827bf5cddbafb3a8c663f6
Hardik Jain <hardik.jain@treebohotels.com>
2018-05-28 12:35:50 +0530

    fix(Gallery): add support for carousel in amp gallery

--- src/Gallery/amp/Gallery.js
+++ src/Gallery/amp/Gallery.js
@@ -3,6 +3,12 @@ import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import GalleryContainer from './GalleryContainer';
 
+const ampCarousel = [{
+  async: '',
+  'custom-element': 'amp-carousel',
+  src: 'https://cdn.ampproject.org/v0/amp-carousel-0.1.js',
+}];
+
 const Gallery = (props) => {
   const {
     width,
@@ -11,17 +17,12 @@ const Gallery = (props) => {
     children,
   } = props;
 
-  const integerWidth = Number.parseInt(width, 10);
-  const integerHeight = Number.parseInt(height, 10);
-
   return (
     <React.Fragment>
-      <Helmet>
-        <script async="" custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js">&nbsp;</script>
-      </Helmet>
+      <Helmet script={ampCarousel} />
       <GalleryContainer
-        width={integerWidth}
-        height={integerHeight}
+        width={width}
+        height={height}
         type={type}
       >
         {children}
--- src/Gallery/amp/GalleryContainer.js
+++ src/Gallery/amp/GalleryContainer.js
@@ -4,14 +4,22 @@ import styled from 'styled-components';
 
 const GalleryContainer = styled(
   ({
+    width,
+    height,
     className,
     ...props
   }) => {
     const carouselProps = {
       layout: 'responsive',
+      controls: '',
+      width: Number.parseInt(width, 10),
+      height: Number.parseInt(height, 10),
       ...props,
     };
-    if (props.type === 'slides') carouselProps.controls = '';
+    if (props.type === 'carousel') {
+      carouselProps.layout = 'fixed-height';
+      carouselProps.width = 'auto';
+    }
     return (
       <amp-carousel
         {...carouselProps}
@@ -19,12 +27,16 @@ const GalleryContainer = styled(
     );
   },
 )`
-  ${''}
+  ${(props) => props.type === 'carousel' ? `
+    .amp-carousel-button {
+      display: none;
+  }
+  ` : ''}
 `;
 
 GalleryContainer.propTypes = {
-  width: PropTypes.number,
-  height: PropTypes.number,
+  width: PropTypes.string,
+  height: PropTypes.string,
   type: PropTypes.oneOf(['carousel', 'slides']).isRequired,
 };
 
