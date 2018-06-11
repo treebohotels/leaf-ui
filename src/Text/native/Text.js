460fc0572d06196fb512436fa8eaf74a870ebaa1
Kashish Grover <kashish.grover@treebohotels.com>
2018-06-11 14:46:05 +0530

    fix(native): enhancers, makeStories and Text now conform to new guidelines (#90)
    * fix(enhancers/native): Fix Flex, Position, Size, Space components for native
    
    * fix(text/native): fix text component for native along with story implementation

--- src/Text/native/Text.js
+++ src/Text/native/Text.js
@@ -1,29 +1,36 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { Platform } from 'react-native';
+import theme from '../../theme';
+
+const isPlatformAndroid = Platform.OS !== 'ios';
+
+const fontFamilies = {
+  normal: isPlatformAndroid ? 'Roboto-Regular' : 'SFProDisplay-Regular',
+  medium: isPlatformAndroid ? 'Roboto-Medium' : 'SFProDisplay-Medium',
+  semibold: isPlatformAndroid ? 'Roboto-Medium' : 'SFProDisplay-Semibold',
+  bold: 'Averta-Bold',
+};
 
 const Text = styled.Text`
-  ${(p) => p.color ? `color: ${p.theme.color[p.color]};` : ''}
-  ${(p) => p.size ? `font-size: ${p.theme.fontSize[p.size]};` : ''}
-  ${(p) => p.weight ? `font-weight: ${p.theme.fontWeight[p.weight]};` : ''}
-  ${(p) => {
-    if (p.family) {
-      return `font-family: ${p.family}`;
-    }
-    return `font-family: ${p.theme.fontFamily[p.weight]}`;
-  }}
-  `;
+  color: ${(props) => props.theme.color[props.color]};
+  font-size: ${(props) => props.theme.fontSize[props.size]};
+  font-family: ${(props) => fontFamilies[props.weight]};
+  text-align: ${(props) => props.align};
+`;
 
 Text.propTypes = {
   color: PropTypes.string,
-  size: PropTypes.string,
-  weight: PropTypes.string,
-  family: PropTypes.string,
-  truncate: PropTypes.bool,
+  size: PropTypes.oneOf(theme.fontSize),
+  weight: PropTypes.oneOf(theme.fontWeight),
+  align: PropTypes.oneOf(['left', 'center', 'right']),
 };
 
 Text.defaultProps = {
-  size: 's',
+  color: 'greyDarker',
+  size: 'm',
   weight: 'normal',
+  align: 'left',
 };
 
 export default Text;
