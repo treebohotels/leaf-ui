bb9d509abc964b839470ed0563b0f9eab0e20eaf
Prachi Rana <prachirana@Prachis-MacBook-Pro.local>
2018-08-21 17:19:31 +0530

    refactor(Checkbox): Removing changes from checkbox makeStories

--- src/Checkbox/makeStories.js
+++ src/Checkbox/makeStories.js
@@ -3,14 +3,10 @@ import React from 'react';
 export default (storiesOf, { Checkbox }) =>
   storiesOf('Checkbox', module)
     .add('simple', () => (
-      <div style={{ width: '100px' }}>
-        <Checkbox
-          name="areLeavesGreen"
-          truncate
-          label="Are leaves green ksrgf ersog esrgfbv ersgibov eiobsrg eiorbv?"
-        />
-      </div>
-
+      <Checkbox
+        name="areLeavesGreen"
+        label="Are leaves green?"
+      />
     ))
     .add('disabled', () => (
       <Checkbox
