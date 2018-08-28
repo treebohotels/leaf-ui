c36f1e6a386a296f6c03aa90478a76c65fbcc4db
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2018-08-28 14:57:45 +0530

    fix(Notification/web): apply new designs and props api

--- src/Notification/makeStories.js
+++ src/Notification/makeStories.js
@@ -2,54 +2,19 @@ import React from 'react';
 
 export default (storiesOf, { Notification }) =>
   storiesOf('Notification', module)
-    .add('simple', () => (
+    .add('success', () => (
       <Notification
-        type="success"
+        color="red"
+        icon="check_circle"
         title="206 Oak upgraded to 205 Mahogany"
-        content="The room was successfully upgraded"
-        actionProps={{
-        onClick: () => {},
-        children: 'OKAY',
-      }}
+        message="The room was successfully upgraded"
+        actions={[{
+          label: 'OKAY',
+          onClick: () => {},
+        }, {
+          label: 'COOL',
+          onClick: () => {},
+        }]}
+        onClose={() => {}}
       />
-    ))
-    .add('types', () => (
-      <React.Fragment>
-        <Notification
-          type="info"
-          title="This is an info message"
-          content="You can upgrade the room anytime"
-          actionProps={{
-          onClick: () => {},
-          children: 'OKAY',
-        }}
-        />
-        <Notification
-          type="success"
-          title="This is a success message"
-          content="The room was successfully upgraded"
-          actionProps={{
-          onClick: () => {},
-          children: 'OKAY',
-        }}
-        />
-        <Notification
-          type="warning"
-          title="This is a warning message"
-          content="Booking closes in few minutes"
-          actionProps={{
-          onClick: () => {},
-          children: 'OKAY',
-        }}
-        />
-        <Notification
-          type="danger"
-          title="This is a danger message"
-          content="Failed to upgrade room!"
-          actionProps={{
-          onClick: () => {},
-          children: 'OKAY',
-        }}
-        />
-      </React.Fragment>
     ));
--- src/Notification/web/NotificationContainer.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import notificationTypes from './notificationTypes';
-
-const NotificationContainer = styled.div`
-  background: ${(props) => props.type ? props.theme.color[`${notificationTypes[props.type].color}Lighter`] : ''};
-  border-top: 2px solid ${(props) => props.type ? props.theme.color[notificationTypes[props.type].color] : ''};
-`;
-
-export default NotificationContainer;
