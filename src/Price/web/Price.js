038cd527bd8632a787c5adc2476e76b543c761ca
Lakshya Ranganath <lakshya.ranganath@gmail.com>
2018-08-31 18:00:53 +0530

    feat(Price/web): conform to new currency standards

--- src/Price/web/Price.js
+++ src/Price/web/Price.js
@@ -1,5 +1,16 @@
 import PropTypes from 'prop-types';
-import formatPrice from './formatPrice';
+
+const formatPrice = (currency, rounded) => {
+  const [amount, code] = currency.split(' ');
+  const price = +amount;
+  const currencyPrice = price.toLocaleString('en-IN', {
+    style: 'currency',
+    currency: code,
+    minimumFractionDigits: rounded ? 0 : 2,
+    maximumFractionDigits: rounded ? 0 : 2,
+  });
+  return currencyPrice;
+};
 
 const Price = ({ rounded, children }) =>
   formatPrice(children, rounded);
@@ -7,10 +18,6 @@ const Price = ({ rounded, children }) =>
 Price.propTypes = {
   rounded: PropTypes.bool,
   children: PropTypes.node,
-};
-
-Price.defaultProps = {
-  rounded: true,
 };
 
 export default Price;
--- src/Price/web/index.js
+++ src/Price/web/index.js
@@ -1,2 +1,1 @@
 export default from './Price';
-export formatPrice from './formatPrice';
