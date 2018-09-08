ab0a972c7d1d98cebd096a89ced86d8e9bda6291
Kashish Grover <kashish.grover@treebohotels.com>
2018-09-08 16:47:19 +0530

    fix(card/native): PR fix - remove no-multi-str rule from eslintrc

--- src/Card/makeStories.js
+++ src/Card/makeStories.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const sampleText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+const sampleText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
 Maecenas at elit eget diam convallis ultricies in a ligula. \
 Nunc rutrum est ut risus laoreet laoreet. \
-Donec convallis massa at pretium tincidunt. \
-Quisque quis tellus quis odio venenatis bibendum.';
+Donec convallis massa at pretium tincidunt.
+Quisque quis tellus quis odio venenatis bibendum.`;
 
 export default (storiesOf, {
   Card,
