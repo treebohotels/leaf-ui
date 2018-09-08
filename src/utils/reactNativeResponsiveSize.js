560a2695f0b5893dacffe7b1d0c68603126a8942
Kashish Grover <kashish.grover@treebohotels.com>
2018-09-08 17:31:05 +0530

    fix(text/native): clean up the util function

--- src/utils/reactNativeResponsiveSize.js
+++ src/utils/reactNativeResponsiveSize.js
@@ -1,20 +1,18 @@
 import { PixelRatio } from 'react-native';
 
 const responsiveSize = (sizePx) => {
-  const pixelRatio = PixelRatio.get(); // https://facebook.github.io/react-native/docs/pixelratio#get
-  const sizeInt = parseInt(sizePx.replace('px', ''), 10);
-  let factor = 1;
+  const pixelRatio = PixelRatio.get();
+  const sizeInt = parseInt(sizePx, 10);
 
+  let factor = 1.4;
   if (pixelRatio <= 1.5) {
     factor = 1;
-  } else if (pixelRatio <= 2 && pixelRatio >= 1.5) {
+  } else if (pixelRatio <= 2) {
     factor = 1.1;
-  } else if (pixelRatio <= 2.5 && pixelRatio >= 2) {
+  } else if (pixelRatio <= 2.5) {
     factor = 1.2;
-  } else if (pixelRatio <= 3 && pixelRatio >= 2.5) {
+  } else if (pixelRatio <= 3) {
     factor = 1.3;
-  } else {
-    factor = 1.4;
   }
 
   return `${sizeInt * factor}px`;
