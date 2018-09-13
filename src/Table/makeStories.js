3ee3c7c177da4ed2554b6b18e1e88927d1218a11
Pragya Jha <pragya2009@gmail.com>
2018-09-13 13:35:38 +0530

    fix(Table/web): remove hover styles from THead, add disabled styles to TR (#111)

--- src/Table/makeStories.js
+++ src/Table/makeStories.js
@@ -12,106 +12,169 @@ export default (storiesOf, {
   Text,
   Select,
   Space,
+  Card,
   Table,
 }) =>
   storiesOf('Table', module)
     .add('default', () => (
-      <Table>
-        <Table.THead>
-          <Table.TR>
-            <Table.TH>Type</Table.TH>
-            <Table.TH>Date</Table.TH>
-            <Table.TH textAlign="right">ID</Table.TH>
-          </Table.TR>
-        </Table.THead>
-        <Table.TBody>
-          <Table.TR>
-            <Table.TD>1000</Table.TD>
-            <Table.TD>2</Table.TD>
-            <Table.TD textAlign="right">Pragya</Table.TD>
-          </Table.TR>
-        </Table.TBody>
-      </Table>
+      <Card>
+        <Table>
+          <Table.THead>
+            <Table.TR>
+              <Table.TH>Type</Table.TH>
+              <Table.TH>Date</Table.TH>
+              <Table.TH textAlign="right">ID</Table.TH>
+            </Table.TR>
+          </Table.THead>
+          <Table.TBody>
+            <Table.TR disabled>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD textAlign="right">Pragya</Table.TD>
+            </Table.TR>
+            <Table.TR>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD textAlign="right">Pragya</Table.TD>
+            </Table.TR>
+            <Table.TR>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD textAlign="right">Pragya</Table.TD>
+            </Table.TR>
+          </Table.TBody>
+        </Table>
+      </Card>
     ))
     .add('with select', () => (
-      <Table>
-        <Table.TBody>
-          <Table.TR>
-            <Table.TH>Type</Table.TH>
-            <Table.TH>Date</Table.TH>
-            <Table.TH>ID</Table.TH>
-            <Table.TH>Paid By</Table.TH>
-            <Table.TH>Paid To</Table.TH>
-            <Table.TH>Mode</Table.TH>
-            <Table.TH>Via</Table.TH>
-            <Table.TH>Amount</Table.TH>
-          </Table.TR>
-          <Table.TR>
-            <Table.TD>1000</Table.TD>
-            <Table.TD>2</Table.TD>
-            <Select
-              name="color"
-              label="Color"
-              options={options}
-              onChange={action('onChange')}
-            />
-            <Table.TD>10/11/2018</Table.TD>
-            <Table.TD>oak</Table.TD>
-            <Table.TD>reserved</Table.TD>
-            <Table.TD>2000</Table.TD>
-            <Table.TD>2000</Table.TD>
-          </Table.TR>
-          <Table.TR>
-            <Table.TD>1000</Table.TD>
-            <Table.TD>2</Table.TD>
-            <Select
-              name="color"
-              label="Color"
-              options={options}
-              onChange={action('onChange')}
-            />
-            <Table.TD>10/11/2018</Table.TD>
-            <Table.TD>oak</Table.TD>
-            <Table.TD>reserved</Table.TD>
-            <Table.TD>2000</Table.TD>
-            <Table.TD>2000</Table.TD>
-          </Table.TR>
-        </Table.TBody>
-      </Table>
+      <Card>
+        <Table>
+          <Table.TBody>
+            <Table.TR>
+              <Table.TH>Type</Table.TH>
+              <Table.TH>Date</Table.TH>
+              <Table.TH>ID</Table.TH>
+              <Table.TH>Paid By</Table.TH>
+              <Table.TH>Paid To</Table.TH>
+              <Table.TH>Mode</Table.TH>
+              <Table.TH>Via</Table.TH>
+              <Table.TH>Amount</Table.TH>
+            </Table.TR>
+            <Table.TR>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD>
+                <Select
+                  name="color"
+                  label="Color"
+                  options={options}
+                  onChange={action('onChange')}
+                />
+              </Table.TD>
+              <Table.TD>10/11/2018</Table.TD>
+              <Table.TD>oak</Table.TD>
+              <Table.TD>reserved</Table.TD>
+              <Table.TD>2000</Table.TD>
+              <Table.TD>2000</Table.TD>
+            </Table.TR>
+            <Table.TR>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD>
+                <Select
+                  name="color"
+                  label="Color"
+                  options={options}
+                  onChange={action('onChange')}
+                />
+              </Table.TD>
+              <Table.TD>10/11/2018</Table.TD>
+              <Table.TD>oak</Table.TD>
+              <Table.TD>reserved</Table.TD>
+              <Table.TD>2000</Table.TD>
+              <Table.TD>2000</Table.TD>
+            </Table.TR>
+          </Table.TBody>
+        </Table>
+      </Card>
     ))
     .add('with tag', () => (
-      <Table>
-        <Table.TBody>
-          <Table.TR>
-            <Table.TH>Type</Table.TH>
-            <Table.TH>Date</Table.TH>
-            <Table.TH>ID</Table.TH>
-            <Table.TH>Paid By</Table.TH>
-            <Table.TH>Paid To</Table.TH>
-            <Table.TH>Mode</Table.TH>
-            <Table.TH>Via</Table.TH>
-            <Table.TH>Amount</Table.TH>
-          </Table.TR>
-          <Table.TR>
-            <Table.TD>1000</Table.TD>
-            <Table.TD>2</Table.TD>
-            <Table.TD>
-              <Text size="s">Oak</Text>
-              <Space margin={[1, 0, 0, 0]}>
-                <Tag
-                  kind="outlined"
-                  shape="capsular"
-                >
-                  Chip
-                </Tag>
-              </Space>
-            </Table.TD>
-            <Table.TD>10/11/2018</Table.TD>
-            <Table.TD>oak</Table.TD>
-            <Table.TD>reserved</Table.TD>
-            <Table.TD>2000</Table.TD>
-            <Table.TD>2000</Table.TD>
-          </Table.TR>
-        </Table.TBody>
-      </Table>
+      <Card>
+        <Table>
+          <Table.THead>
+            <Table.TR>
+              <Table.TH>Type</Table.TH>
+              <Table.TH>Date</Table.TH>
+              <Table.TH>ID</Table.TH>
+              <Table.TH>Paid By</Table.TH>
+              <Table.TH>Paid To</Table.TH>
+              <Table.TH>Mode</Table.TH>
+              <Table.TH>Via</Table.TH>
+              <Table.TH>Amount</Table.TH>
+            </Table.TR>
+          </Table.THead>
+          <Table.TBody>
+            <Table.TR disabled>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD>
+                <Text size="s">Oak</Text>
+                <Space margin={[1, 0, 0, 0]}>
+                  <Tag
+                    kind="outlined"
+                    shape="capsular"
+                  >
+                    Chip
+                  </Tag>
+                </Space>
+              </Table.TD>
+              <Table.TD>10/11/2018</Table.TD>
+              <Table.TD>oak</Table.TD>
+              <Table.TD>reserved</Table.TD>
+              <Table.TD>2000</Table.TD>
+              <Table.TD>2000</Table.TD>
+            </Table.TR>
+            <Table.TR onClick={action('onClick')}>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD>
+                <Text size="s">Oak</Text>
+                <Space margin={[1, 0, 0, 0]}>
+                  <Tag
+                    kind="outlined"
+                    shape="capsular"
+                  >
+                    Chip
+                  </Tag>
+                </Space>
+              </Table.TD>
+              <Table.TD>10/11/2018</Table.TD>
+              <Table.TD>oak</Table.TD>
+              <Table.TD>reserved</Table.TD>
+              <Table.TD>2000</Table.TD>
+              <Table.TD>2000</Table.TD>
+            </Table.TR>
+            <Table.TR onClick={action('onClick')} disabled>
+              <Table.TD>1000</Table.TD>
+              <Table.TD>2</Table.TD>
+              <Table.TD>
+                <Text size="s">Oak</Text>
+                <Space margin={[1, 0, 0, 0]}>
+                  <Tag
+                    kind="outlined"
+                    shape="capsular"
+                  >
+                    Chip
+                  </Tag>
+                </Space>
+              </Table.TD>
+              <Table.TD>10/11/2018</Table.TD>
+              <Table.TD>oak</Table.TD>
+              <Table.TD>reserved</Table.TD>
+              <Table.TD>2000</Table.TD>
+              <Table.TD>2000</Table.TD>
+            </Table.TR>
+          </Table.TBody>
+        </Table>
+      </Card>
     ));
