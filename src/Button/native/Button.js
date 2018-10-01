c33bcdc82450dbbcbbf8b067cf16437466d4310f
Kashish Grover <kashish.grover@treebohotels.com>
2018-10-01 12:43:50 +0530

    fix(Button/native): Properly implement disabled button and loading state

--- src/Button/native/Button.js
+++ src/Button/native/Button.js
@@ -7,13 +7,28 @@ import Text from '../../Text/native';
 
 const styles = {
   buttonColor(props) {
-    if (props.kind === 'filled' && props.disabled) {
-      return `${props.theme.color[props.color]}aa`;
-    }
-    if (props.kind === 'filled') {
+    if (props.kind === 'filled' && !props.disabled) {
       return props.theme.color[props.color];
     }
+    if (props.kind === 'filled' && props.disabled) {
+      return props.theme.color.grey;
+    }
+    if (props.kind === 'outlined' && props.disabled) {
+      return props.theme.color.greyLight;
+    }
     return props.theme.color.white;
+  },
+  borderColor(props) {
+    if (props.kind === 'filled' && !props.disabled) {
+      return props.theme.color[props.color];
+    }
+    if (props.kind === 'filled' && props.disabled) {
+      return props.theme.color.grey;
+    }
+    if (props.kind === 'outlined' && props.disabled) {
+      return props.theme.color.grey;
+    }
+    return props.theme.color[props.color];
   },
   padding(props) {
     if (props.size === 'small') {
@@ -33,6 +48,15 @@ const styles = {
     }
     return props.theme.px(10);
   },
+  textColor(props) {
+    if (props.kind === 'filled') {
+      return props.theme.color.white;
+    }
+    if (props.kind === 'outlined' && props.disabled) {
+      return props.theme.color.grey;
+    }
+    return props.theme.color[props.color];
+  },
 };
 
 const StyledButton = styled.TouchableOpacity`
@@ -42,8 +66,12 @@ const StyledButton = styled.TouchableOpacity`
   padding: ${styles.padding};
   border-width: 1px;
   border-style: solid;
-  border-color: ${(props) => props.theme.color[props.color]};
+  border-color: ${styles.borderColor};
   border-radius: ${styles.borderRadius};
+`;
+
+const StyledText = styled(Text)`
+  color: ${styles.textColor};
 `;
 
 const Button = ({
@@ -64,19 +92,25 @@ const Button = ({
     shape={shape}
     block={block}
     disabled={disabled}
+    activeOpacity={0.8}
     {...props}
   >
     {
-      isLoading ?
-        <ActivityIndicator color={theme.color.white} /> : (
-          <Text
-            color={kind === 'filled' ? 'white' : 'primary'}
-            size={size === 'small' ? 'xs' : 's'}
-            weight="medium"
-          >
-            {children.toUpperCase()}
-          </Text>
-        )
+      isLoading ? (
+        <ActivityIndicator
+          color={kind === 'outlined' ? theme.color[color] : theme.color.white}
+        />
+      ) : (
+        <StyledText
+          kind={kind}
+          color={color}
+          disabled={disabled}
+          size={size === 'small' ? 'xs' : 's'}
+          weight="medium"
+        >
+          {children.toUpperCase()}
+        </StyledText>
+      )
     }
   </StyledButton>
 );
