79b806137e5f279344716edf658aa6731f03122d
Kashish Grover <kashish.grover@treebohotels.com>
2018-10-01 13:19:24 +0530

    fix(View/native): Re export plain React Native View as Leaf UI View

--- src/View/native/View.js
+++ src/View/native/View.js
@@ -1,25 +1,3 @@
-import React from 'react';
-import { View as ReactNativeView } from 'react-native';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const View = styled(
-  ({
-    component,
-    children,
-    ...props
-  }) => React.createElement(component, props, children),
-)`
-  ${' '}
-`;
-
-View.propTypes = {
-  component: PropTypes.node.isRequired,
-  children: PropTypes.node,
-};
-
-View.defaultProps = {
-  component: ReactNativeView,
-};
+import { View } from 'react-native';
 
 export default View;
