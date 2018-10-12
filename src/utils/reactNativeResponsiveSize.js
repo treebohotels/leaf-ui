712114e1798ebc6e03bbf8da43e6d493debf4614
Kashish Grover <kashish.grover@treebohotels.com>
2018-10-12 13:32:45 +0530

    fix(responsiveSize/native): try different formula (#127)

--- src/utils/reactNativeResponsiveSize.js
+++ src/utils/reactNativeResponsiveSize.js
@@ -1,7 +1,17 @@
-import { scale } from 'react-native-size-matters';
+import { Dimensions } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('screen').height;
+const SCREEN_WIDTH = Dimensions.get('screen').width;
+
+const baseHeight = 568;
+const baseWidth = 320;
+
+const verticalScale = SCREEN_HEIGHT / baseHeight;
+const horizontalScale = SCREEN_WIDTH / baseWidth;
+
+const scale = (size) => ((horizontalScale / verticalScale) * size);
 
 export const responsiveSizeInt = (size) => scale(size);
-
 export const responsiveSizePx = (sizePx) => {
   const sizeInt = parseInt(sizePx, 10);
   return `${scale(sizeInt)}px`;
