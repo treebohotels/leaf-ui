abcdfefcfcca42d5da4f557ea664d1b7379bb015
Peri Nikhil <peri.nikhil@treebohotels.comr>
2018-11-12 15:40:04 +0530

    feat(Table): THead: make sticky

--- src/Table/web/THead.js
+++ src/Table/web/THead.js
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
+import TH from './TH';
 import TR from './TR';
 
 const THead = styled.thead`
-  ${/* sc-selector */TR} {
+  ${/* sc-selector */TH} {
+    ${(props) => props.sticky ? `
+      background-color: ${props.theme.color.white};
+      box-shadow: 0px 1px ${props.theme.color.greyLight};
+      position: sticky;
+      top: 0;
+      z-index: 1100;
+    ` : ''}
+  }
+
+  ${/* sc-custom */TR} {
     &:hover {
       background-color: initial;
     }
