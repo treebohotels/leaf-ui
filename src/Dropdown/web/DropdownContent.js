d711b04559d1409a59c5463416f10beeecebffb9
Varsha Bhagat <36624782+VarshaBhagat0714@users.noreply.github.com>
2018-12-06 14:47:13 +0530

    fix(Dropdown): remove the arrow (#143)

--- src/Dropdown/web/DropdownContent.js
+++ src/Dropdown/web/DropdownContent.js
@@ -8,75 +8,17 @@ class DropdownContent extends React.Component {
   styles = {
     dropdown: {
       position: 'absolute',
-      padding: this.props.theme.px(1),
-    },
-
-    dropdownArrow: {
-      position: 'absolute',
-      width: 0,
-      height: 0,
-      borderRightColor: this.props.theme.color.transparent,
-      borderLeftColor: this.props.theme.color.transparent,
-      borderTopColor: this.props.theme.color.transparent,
-      borderBottomColor: this.props.theme.color.transparent,
-      borderStyle: 'solid',
-    },
-
-    placement: {
-      top: {
-        arrow: {
-          bottom: 1,
-          marginLeft: this.props.theme.px(-1),
-          borderWidth: this.props.theme.px([1, 1, 0]),
-          borderTopColor: this.props.theme.color.white,
-        },
-      },
-      right: {
-        arrow: {
-          left: 1,
-          marginTop: this.props.theme.px(-1),
-          borderWidth: this.props.theme.px([1, 1, 1, 0]),
-          borderRightColor: this.props.theme.color.white,
-        },
-      },
-      bottom: {
-        arrow: {
-          top: 1,
-          marginLeft: this.props.theme.px(-1),
-          borderWidth: this.props.theme.px([0, 1, 1]),
-          borderBottomColor: this.props.theme.color.white,
-        },
-      },
-      left: {
-        arrow: {
-          right: 1,
-          marginTop: this.props.theme.px(-1),
-          borderWidth: this.props.theme.px([1, 0, 1, 1]),
-          borderLeftColor: this.props.theme.color.white,
-        },
-      },
     },
   }
 
   render() {
     const {
       style,
-      placement,
-      arrowOffsetLeft: left = this.styles.placement[placement].arrow.left,
-      arrowOffsetTop: top = this.styles.placement[placement].arrow.top,
       children,
     } = this.props;
 
     return (
       <div style={{ ...this.styles.dropdown, ...style }}>
-        <div
-          style={{
-            ...this.styles.dropdownArrow,
-            ...this.styles.placement[placement].arrow,
-            left,
-            top,
-          }}
-        />
         <Size minWidth={25}>
           <Card borderStyle="none" elevated>
             {children}
@@ -88,11 +30,7 @@ class DropdownContent extends React.Component {
 }
 
 DropdownContent.propTypes = {
-  theme: PropTypes.object,
   style: PropTypes.object,
-  placement: PropTypes.string,
-  arrowOffsetLeft: PropTypes.string,
-  arrowOffsetTop: PropTypes.string,
   children: PropTypes.node,
 };
 
