815f5b38cc09a2a1af455d07d0f2ad5cc739a0dc
Kashish Grover <kashish.grover@treebohotels.com>
2019-02-11 18:07:27 +0530

    fix(native/tag): fix import

--- src/Tag/native/Tag.js
+++ src/Tag/native/Tag.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import Text from '../../Text/native';
 
 const styles = {
